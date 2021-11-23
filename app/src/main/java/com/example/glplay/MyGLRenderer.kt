dca278384cb60c0f2ac10356f1feb46345352a9c
Prempal Singh <prempal.42@gmail.com>
2021-11-24 03:03:26 +0530

    draw triangle

--- app/src/main/java/com/example/glplay/MyGLRenderer.kt
+++ app/src/main/java/com/example/glplay/MyGLRenderer.kt
@@ -12,16 +12,23 @@ class MyGLRenderer : GLSurfaceView.Renderer {
         private const val TAG = "MyGLRenderer"
     }
 
+    private lateinit var mTriangle: Triangle
+    private lateinit var mSquare: Square
+
     override fun onSurfaceCreated(unused: GL10, config: EGLConfig) {
         Log.d(TAG, "onSurfaceCreated: ")
         // Set the background frame color
-        GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f)
+        GLES20.glClearColor(1.0f, 0.0f, 0.0f, 1.0f)
+
+        mTriangle = Triangle()
+        mSquare = Square()
     }
 
     override fun onDrawFrame(unused: GL10) {
         Log.d(TAG, "onDrawFrame: ")
         // Redraw background color
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
+        mTriangle.draw()
     }
 
     override fun onSurfaceChanged(unused: GL10, width: Int, height: Int) {
