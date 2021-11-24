ee7ac9c8660fc71a98eea3e203131283c96e82fc
Prempal Singh <prempal.42@gmail.com>
2021-11-24 13:29:19 +0530

    apply projection and camera view

--- app/src/main/java/com/example/glplay/MyGLRenderer.kt
+++ app/src/main/java/com/example/glplay/MyGLRenderer.kt
@@ -2,6 +2,7 @@ package com.example.glplay
 
 import android.opengl.GLES20
 import android.opengl.GLSurfaceView
+import android.opengl.Matrix
 import android.util.Log
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
@@ -11,6 +12,11 @@ class MyGLRenderer : GLSurfaceView.Renderer {
     companion object {
         private const val TAG = "MyGLRenderer"
     }
+
+    // vPMatrix is an abbreviation for "Model View Projection Matrix"
+    private val vPMatrix = FloatArray(16)
+    private val projectionMatrix = FloatArray(16)
+    private val viewMatrix = FloatArray(16)
 
     private lateinit var mTriangle: Triangle
     private lateinit var mSquare: Square
@@ -28,11 +34,24 @@ class MyGLRenderer : GLSurfaceView.Renderer {
         Log.d(TAG, "onDrawFrame: ")
         // Redraw background color
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
-        mTriangle.draw()
+
+        // Set the camera position (View matrix)
+        Matrix.setLookAtM(viewMatrix, 0, 0f, 0f, -3f, 0f, 0f, 0f, 0f, 1.0f, 0.0f)
+
+        // Calculate the projection and view transformation
+        Matrix.multiplyMM(vPMatrix, 0, projectionMatrix, 0, viewMatrix, 0)
+
+        mTriangle.draw(vPMatrix)
     }
 
     override fun onSurfaceChanged(unused: GL10, width: Int, height: Int) {
         Log.d(TAG, "onSurfaceChanged: ")
         GLES20.glViewport(0, 0, width, height)
+
+        val ratio: Float = width.toFloat() / height.toFloat()
+
+        // this projection matrix is applied to object coordinates
+        // in the onDrawFrame() method
+        Matrix.frustumM(projectionMatrix, 0, -ratio, ratio, -1f, 1f, 3f, 7f)
     }
 }
--- app/src/main/java/com/example/glplay/Shapes.kt
+++ app/src/main/java/com/example/glplay/Shapes.kt
@@ -31,9 +31,15 @@ class Triangle {
     private var mProgram: Int
 
     private val vertexShaderCode =
-        "attribute vec4 vPosition;" +
+    // This matrix member variable provides a hook to manipulate
+        // the coordinates of the objects that use this vertex shader
+        "uniform mat4 uMVPMatrix;" +
+            "attribute vec4 vPosition;" +
             "void main() {" +
-            "  gl_Position = vPosition;" +
+            // the matrix must be included as a modifier of gl_Position
+            // Note that the uMVPMatrix factor *must be first* in order
+            // for the matrix multiplication product to be correct.
+            "  gl_Position = uMVPMatrix * vPosition;" +
             "}"
 
     private val fragmentShaderCode =
@@ -82,10 +88,13 @@ class Triangle {
     private var positionHandle: Int = 0
     private var mColorHandle: Int = 0
 
+    // Use to access and set the view transformation
+    private var vPMatrixHandle: Int = 0
+
     private val vertexCount: Int = triangleCoords.size / COORDS_PER_VERTEX
     private val vertexStride: Int = COORDS_PER_VERTEX * 4 // 4 bytes per vertex
 
-    fun draw() {
+    fun draw(mvpMatrix: FloatArray) {
         // Add program to OpenGL ES environment
         GLES20.glUseProgram(mProgram)
 
@@ -111,6 +120,18 @@ class Triangle {
                 // Set color for drawing the triangle
                 GLES20.glUniform4fv(colorHandle, 1, color, 0)
             }
+
+            // get handle to shape's transformation matrix
+            vPMatrixHandle = GLES20.glGetUniformLocation(mProgram, "uMVPMatrix")
+
+            // Pass the projection and view transformation to the shader
+            GLES20.glUniformMatrix4fv(vPMatrixHandle, 1, false, mvpMatrix, 0)
+
+            // Draw the triangle
+            GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, vertexCount)
+
+            // Disable vertex array
+            GLES20.glDisableVertexAttribArray(positionHandle)
 
             // Draw the triangle
             GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, vertexCount)
